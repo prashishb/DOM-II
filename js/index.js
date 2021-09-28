ce613dc106055c88d1ba8814c9055dacf4a51db1
prashishb <prashb.web@gmail.com>
2021-09-28 16:02:33 -0400

    bg color change on double click

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 
 const navLinks = document.querySelectorAll('.nav a');
 let headerImg = document.querySelector('header img');
+const body = document.querySelector('body');
 
 // mouseover / mouseout event
 navLinks.forEach(elem => elem.addEventListener('mouseover', () => {
@@ -18,3 +19,12 @@ window.addEventListener('load', () => alert("I am a popup!!!!"));
 // random header image onclick
 headerImg.addEventListener('click', () => headerImg.src = 'https://source.unsplash.com/random/800x250');
 
+// doubleclick event to change bg color
+window.addEventListener('dblclick', () => {
+    console.log('pineapples');
+    if (body.style.backgroundColor === '') {
+        body.style.backgroundColor = 'DarkSeaGreen';
+    } else {
+        body.style.backgroundColor = '';
+    }
+});
