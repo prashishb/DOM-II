ac3d352696b0822bb7a6f2cc9ca1a32d626a747f
prashishb <prashb.web@gmail.com>
2021-09-28 14:33:46 -0400

    added a mouseover event to underline navlinks on hover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+const navLinks = document.querySelectorAll('.nav a');
+console.log(navLinks);
+
+// mouseover / mouseout event
+navLinks.forEach(elem => elem.addEventListener('mouseover', () => elem.style.textDecoration = 'underline'));
+
+navLinks.forEach(elem => elem.addEventListener('mouseout', () => elem.style.textDecoration = 'none'));
+
