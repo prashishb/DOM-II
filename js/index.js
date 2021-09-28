3cc11f2cb504cf9305646285ecb242a6b99359a0
prashishb <prashb.web@gmail.com>
2021-09-28 16:52:38 -0400

    nav bg color change when scrolling to bottom of page

--- js/index.js
+++ js/index.js
@@ -4,27 +4,40 @@ const navLinks = document.querySelectorAll('.nav a');
 let headerImg = document.querySelector('header img');
 const body = document.querySelector('body');
 
-// mouseover / mouseout event
+// mouseover event underline navLinks style
 navLinks.forEach(elem => elem.addEventListener('mouseover', () => {
     elem.style.textDecoration = 'underline';
     elem.style.textDecorationThickness = '0.5rem';
     elem.style.textDecorationColor = '#f44336';
 }));
 
+// mouseout event default navLinks styles
 navLinks.forEach(elem => elem.addEventListener('mouseout', () => elem.style.textDecoration = 'none'));
 
 // popup on page load
 window.addEventListener('load', () => alert("I am a popup!!!!"));
 
-// random header image onclick
+// onclick event generate random header image
 headerImg.addEventListener('click', () => headerImg.src = 'https://source.unsplash.com/random/800x250');
 
 // doubleclick event to change bg color
 window.addEventListener('dblclick', () => {
     console.log('pineapples');
     if (body.style.backgroundColor === '') {
-        body.style.backgroundColor = 'DarkSeaGreen';
+        body.style.backgroundColor = 'AliceBlue';
     } else {
         body.style.backgroundColor = '';
     }
 });
+
+// nav bg color change when scrolled to bottom of page
+document.addEventListener('scroll', () => {
+    let documentHeight = document.body.scrollHeight;
+    let currScroll = window.scrollY + window.innerHeight;
+    let modifier = 200;
+    if (currScroll + modifier > documentHeight) {
+        document.querySelector('.main-navigation').style.background = '#FFEBCD';
+    } else {
+        document.querySelector('.main-navigation').style.background = '#FFFFFF';
+    }
+})
