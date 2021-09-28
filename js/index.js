b05d61930a900279c47b2a70bc260fc4c7437f79
prashishb <prashb.web@gmail.com>
2021-09-28 15:49:35 -0400

    added underline styling to navlinks on mouseover

--- js/index.js
+++ js/index.js
@@ -4,7 +4,11 @@ const navLinks = document.querySelectorAll('.nav a');
 let headerImg = document.querySelector('header img');
 
 // mouseover / mouseout event
-navLinks.forEach(elem => elem.addEventListener('mouseover', () => elem.style.textDecoration = 'underline'));
+navLinks.forEach(elem => elem.addEventListener('mouseover', () => {
+    elem.style.textDecoration = 'underline';
+    elem.style.textDecorationThickness = '0.5rem';
+    elem.style.textDecorationColor = '#f44336';
+}));
 
 navLinks.forEach(elem => elem.addEventListener('mouseout', () => elem.style.textDecoration = 'none'));
 
@@ -13,3 +17,4 @@ window.addEventListener('load', () => alert("I am a popup!!!!"));
 
 // random header image onclick
 headerImg.addEventListener('click', () => headerImg.src = 'https://source.unsplash.com/random/800x250');
+
