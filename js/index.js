58e72a55d695290ddd95d0d160c73fe0be03e85c
prashishb <prashb.web@gmail.com>
2021-09-28 15:23:59 -0400

    added a page load and onclick event

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 // Your code goes here
 
 const navLinks = document.querySelectorAll('.nav a');
-console.log(navLinks);
+let headerImg = document.querySelector('header img');
 
 // mouseover / mouseout event
 navLinks.forEach(elem => elem.addEventListener('mouseover', () => elem.style.textDecoration = 'underline'));
 
 navLinks.forEach(elem => elem.addEventListener('mouseout', () => elem.style.textDecoration = 'none'));
 
+// popup on page load
+window.addEventListener('load', () => alert("I am a popup!!!!"));
+
+// random header image onclick
+headerImg.addEventListener('click', () => headerImg.src = 'https://source.unsplash.com/random/800x250');
